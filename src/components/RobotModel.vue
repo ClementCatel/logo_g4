037d800c7df3befb09207f334312e7aa3667e1c7
MalronWall <thibaut.tourte17@gmail.com>
2020-11-10 09:16:13 +0100

    :construction: : Preparation of the View for the reception of the model with threejs

---
+++ src/components/RobotModel.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-12">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RobotModel',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <RobotModel />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import RobotModel from '@/components/RobotModel.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+      RobotModel
   }
 }
 </script>
